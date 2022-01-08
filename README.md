bcfe1842b158f5de9864b50363c7bd8c7587648c
Courtney Foster <cfoster649@gmail.com>
2022-01-07 21:58:18 -0800

    Fixed bug not saving 0800 and 0900 time blocks

--- README.md
+++ README.md
@@ -1,1 +1,53 @@
-# daily-planner
+# Daily Scheduler
+
+The page shows working hours divided up into 1 hour incriments, color coded based on past hours, current hours, and future hours. User is able to input tasks into each hourly block and save them to local storage. Tasks will remain on the page with page refresh. Current date and time is displayed at the top of the page.
+
+## Getting Started
+
+* User will enter page and see time blocks for hours 0800-1800
+    * Time block(s) colored grey are past hours
+    * Time block colored red is current hour
+    * Time block(s) colored green are future hours
+* User can enter tasks into each specific time block and save when clicking "Save" button corresponding to time block
+* All saved inputs will remain on the page with page refresh
+
+
+## Essential Code
+***images/gifs
+
+
+
+## Built With
+
+* [HTML](https://developer.mozilla.org/en-US/docs/Web/HTML)
+* [CSS](https://developer.mozilla.org/en-US/docs/Web/CSS)
+* [Javascript](https://developer.mozilla.org/en-US/docs/Web/JavaScript)
+* [JQuery](https://developer.mozilla.org/en-US/docs/Glossary/jQuery)
+
+## Deployed Link
+
+* [See Live Site](https://cfoster121.github.io/daily-planner/)
+
+
+## Author
+
+**Courtney Foster** 
+
+- [Link to Portfolio Site](https://cfoster121.github.io/homework2/)
+- [Link to Github](https://github.com/cfoster121)
+- [Link to LinkedIn](https://www.linkedin.com/in/courtney-foster-0b364575/)
+
+See also the list of [contributors](https://github.com/your/project/contributors) who participated in this project.
+
+
+## Acknowledgments
+
+* Hat tip to anyone whose code, libraries, packages, or UI was used  / inspired from
+* Inspiration
+* etc
+* [W3 Schools](https://www.w3schools.com/)
+* [MDN Web Docs](https://developer.mozilla.org/en-US/)
+* [Gif Application](https://gifox.io/)
+* [Markdown Preview](https://dillinger.io/)
+* Slack study groups
+* Tutor/TA sessions
--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 //Display Dynamic Time on Page
 function displayTime() {
     let currentTime = moment().format('MMMM Do YYYY, HH:mm:ss');
@@ -14,22 +13,22 @@ function colorCoordinate() {
     let timeHours = moment().format('HH');
 
     if (timeHours == 08) {
-        $("#t8").addClass("present")
+        $("#t08").addClass("present")
     }
     else if (timeHours > 08) {
-        $("#t8").addClass("past")
+        $("#t08").addClass("past")
     }
     else {
-        $("#t8").addClass("future")
+        $("#t08").addClass("future")
     }
     if (timeHours == 09) {
-        $("#t9").addClass("present")
+        $("#t09").addClass("present")
     }
     else if (timeHours > 09) {
-        $("#t9").addClass("past")
+        $("#t09").addClass("past")
     }
     else {
-        $("#t9").addClass("future")
+        $("#t09").addClass("future")
     }
     if (timeHours == 10) {
         $("#t10").addClass("present")
@@ -115,6 +114,7 @@ function colorCoordinate() {
 }
 colorCoordinate()
 
+
 //Show local storage values in time blocks on page load
 $("#t08").val(window.localStorage.getItem("Task08"));
 $("#t09").val(window.localStorage.getItem("Task09"));
@@ -129,14 +129,13 @@ $("#t17").val(window.localStorage.getItem("Task17"));
 $("#t18").val(window.localStorage.getItem("Task18"));
 
 
-
 //On click of each save button, text area for associated time block is stored into unique local storage area specific to time
 $("#s08").click(function(){
-    let taskValue = $("#t8").val();
+    let taskValue = $("#t08").val();
     localStorage.setItem("Task08", taskValue);
 });
 $("#s09").click(function(){
-    let taskValue = $("#t9").val();
+    let taskValue = $("#t09").val();
     localStorage.setItem("Task09", taskValue);
 });
 $("#s10").click(function(){
@@ -175,62 +174,3 @@ $("#s18").click(function(){
     let taskValue = $("#t18").val();
     localStorage.setItem("Task18", taskValue);
 });
-
-
-// ****************Step 1*******************************
-// - What is the Step
-// - Display current date/time at top of page
-// - Why am i going to do it
-// - Satisfy acceptance criteria 1
-// - How am I going to do it
-// - Moment.js with setinterval function to display dynamic date/time
-//
-// ****************Step 2*******************************
-// - What is the Step
-// - Create time blocks on page from 0800 to 1800 (10 blocks)
-// - Why am i going to do it 
-// - Satisfy acceptance criteria 2 (organize day in 1 hr sections)
-// - How am I going to do it
-// - Use bootstrap rows/columns
-//
-// ****************Step 3*******************************
-// - What is the Step
-// - Set timeblocks to change colors as time passes
-// - Why am i going to do it
-// - Indicate whether time block is current, future, or past
-// - How am I going to do it
-// - Set up function relating each time block to change color based on current time displayed in original dynamic date/time function
-//
-// ****************Step 4*******************************
-// - What is the Step
-// - Save tasks when clicking save button
-// - Why am i going to do it
-// - Save entered tasks to local storage
-// - How am I going to do it
-// - Create sections in local storage corresponding to time block sections that store data entered in clickable boxes from step 4
-//
-// Step 5
-// - What is the Step
-// - Keep saved tasks on page on refresh 
-// - Why am i going to do it
-// - To keep tasks from deleting if page is refreshed 
-// - How am I going to do it
-// - Use local storage to display saved item on page load
-
-
-
-//## Acceptance Criteria
-
-//GIVEN I am using a daily planner to create a schedule
-//WHEN I open the planner
-//THEN the current day is displayed at the top of the calendar
-//WHEN I scroll down
-//THEN I am presented with timeblocks for standard business hours
-//WHEN I view the timeblocks for that day
-//THEN each timeblock is color coded to indicate whether it is in the past, present, or future
-//WHEN I click into a timeblock
-//THEN I can enter an event
-//WHEN I click the save button for that timeblock
-//THEN the text for that event is saved in local storage
-//WHEN I refresh the page
-//THEN the saved events persist
